ae9228fbcdf02fc5b5906288b7e02c0688cf0f98
Arsen Baktiyarov <baktiyarov21@gmail.com>
2020-12-23 12:48:18 +0600

    Finished creation DataBase.java

--- app/src/main/java/arsen/baktiyarov/todo/DataBase.java
+++ app/src/main/java/arsen/baktiyarov/todo/DataBase.java
@@ -1,10 +1,15 @@
 package arsen.baktiyarov.todo;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 public class DataBase extends SQLiteOpenHelper {
 
@@ -20,12 +25,44 @@ public class DataBase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY, %s TEXT  NOT NULL)",db_table,db_column);
-
+        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT  NOT NULL)", db_table, db_column);
+        sqLiteDatabase.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String query = String.format("DELETE TABLE IF EXISTS %s", db_table);
+        sqLiteDatabase.execSQL(query);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void insert(String task) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(db_column, task);
+        db.insertWithOnConflict(db_table, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+    }
+
+    public void delete(String task) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(db_table, db_column + " = ?", new String[]{task});
+        db.close();
+    }
+
+    public ArrayList<String> allTasks() {
+        ArrayList<String> allTasks = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(db_table, new String[]{db_column}, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int index = cursor.getColumnIndex(db_column);
+            allTasks.add(cursor.getString(index));
+
+        }
+        cursor.close();
+        db.close();
+        return allTasks;
+
 
     }
+
 }
--- app/src/main/java/arsen/baktiyarov/todo/MainActivity.java
+++ app/src/main/java/arsen/baktiyarov/todo/MainActivity.java
@@ -2,13 +2,39 @@ package arsen.baktiyarov.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private String message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        message = editText.getText().toString();
+        OnClick();
+    }
+
+    public void OnClick() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT,message);
+                Intent chooseIntent = Intent.createChooser(intent,"Отправить сообщение");
+                startActivity(chooseIntent);
+            }
+        });
     }
 }
